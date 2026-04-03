f054bd58c6669e132f0071a67ad835fc22b8ce1f
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-04-03 18:20:08 +0800

    [ax_custom_rom_hide] adding adb props to spoof list
    Change-Id: Iec9f69aa4f53ff1c6325bc5ba5f0cf437af224d3
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- libc/bionic/custom_rom_hide.cpp
+++ libc/bionic/custom_rom_hide.cpp
@@ -515,8 +515,8 @@ static const char* const kSpoofedEmptyProps[] = {
     "ro.lineage.build.version",
     "org.voltage.version",
     "ro.modversion",
-    "service.adb.root",
-    "init.svc.adb_root",
+    "init.svc_debug_pid.adb_root",
+    "init.svc_debug_pid.adbd",
     nullptr
 };
 
@@ -528,8 +528,13 @@ struct PropOverride {
 static const PropOverride kSpoofedValueProps[] = {
     {"ro.debuggable", "0"},
     {"ro.build.type", "user"},
+    {"ro.secure", "1"},
     {"ro.adb.secure", "1"},
     {"persist.sys.usb.config", "mtp"},
+    {"service.adb.root", "0"},
+    {"init.svc.adb_root", "stopped"},
+    {"init.svc.adbd", "stopped"},
+    {"sys.usb.config", "mtp"},
     {nullptr, nullptr}
 };
 
